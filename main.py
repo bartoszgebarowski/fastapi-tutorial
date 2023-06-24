86324339024eeb5a252366fd44fe55b7e364df5b
bartoszgebarowski <gebarowski.bartosz@gmail.com>
2023-06-25 00:09:14 +0200

    add request body section

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -117,3 +118,31 @@ async def get_user_item(
     if not short:
         item.update({"description": "Lorem ipsum dolor"})
     return item
+
+
+# Request body section
+
+
+class Item(BaseModel):
+    name: str
+    description: str
+    price: float
+    # Optional |
+    tax: float | None = None
+
+
+@app.post("/items")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
