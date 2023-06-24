7be771e07f446156ea2535c1c055c4e0f0dd69c7
bartoszgebarowski <gebarowski.bartosz@gmail.com>
2023-06-24 21:56:04 +0200

    add query parameters section

--- main.py
+++ main.py
@@ -29,15 +29,15 @@ async def put():
     return {"message": "hello from the put route"}
 
 
-@app.get("/items")
-async def list_items():
-    return {"message": "list items route"}
+# @app.get("/items")
+# async def list_items():
+#     return {"message": "list items route"}
 
 
-@app.get("/items/{item_id}")
-# Specify an expected type of data
-async def get_item(item_id: int):
-    return {"item_id": item_id}
+# @app.get("/items/{item_id}")
+# # Specify an expected type of data
+# async def get_item(item_id: int):
+#     return {"item_id": item_id}
 
 
 # Path parameters section
@@ -77,3 +77,43 @@ async def get_food(food_name: FoodEnum):
             "message": "you are still healthy, but like other things",
         }
     return {"food_name": food_name, "message": "i like gouda"}
+
+
+# Query parameters section
+
+
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Bus"},
+]
+
+
+@app.get("/items")
+async def list_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+# required query parameter sample_query_parameter
+async def get_item(
+    item_id: str, sample_query_param, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "sample_query_param": sample_query_param}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "Lorem ipsum dolor"})
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def get_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "Lorem ipsum dolor"})
+    return item
